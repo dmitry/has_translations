36225c84656947665f8f187ab51bae2be21abd7e
Alexander Shlinchak <ashlinchak@gmail.com>
2016-07-12 16:44:05 +0300

    Add foreign_key for associations (#19)
    * Add the `foreign_key` option
    * Update documentation for `foreign_key`

--- test/schema.rb
+++ test/schema.rb
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(:version => 0) do
     t.string :locale, :null => false, :limit => 2
     t.text :text
   end
+
+  create_table :organizations, :force => true do |t|
+    t.string :title
+  end
+
+  create_table :organization_translations, :force => true do |t|
+    t.integer :company_id, :null => false
+    t.string :locale, :null => false, :limit => 2
+    t.text :text
+  end
 end
 
 class ArticleTranslation < ActiveRecord::Base
@@ -36,4 +46,12 @@ class Team < ActiveRecord::Base
   include HasTranslations::ModelAdditions
 
   has_translations :text, :fallback => true, :nil => nil
+end
+
+class OrganizationTranslation < ActiveRecord::Base
+end
+class Organization < ActiveRecord::Base
+  include HasTranslations::ModelAdditions
+
+  has_translations :text, :writer => true, :foreign_key => 'company_id'
 end
